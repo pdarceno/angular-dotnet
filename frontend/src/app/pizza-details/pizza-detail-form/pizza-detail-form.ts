41fd29160bec13bb949fe11c9ac04cf1c74689da
Prosy Arceno <pdarceno@gmail.com>
2025-06-08 04:28:14 +0800

    feat: add pizza-details component

---
+++ frontend/src/app/pizza-details/pizza-detail-form/pizza-detail-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pizza-detail-form',
+  imports: [],
+  templateUrl: './pizza-detail-form.html',
+  styles: ``
+})
+export class PizzaDetailForm {
+
+}
---
+++ frontend/src/app/pizza-details/pizza-details.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pizza-details',
+  imports: [],
+  templateUrl: './pizza-details.html',
+  styles: ``
+})
+export class PizzaDetails {
+
+}
