1206a8d3bac4decb4d96c1837ca80afc8cdbfbdb
Prosy Arceno <pdarceno@gmail.com>
2025-06-08 14:42:16 +0800

    feat: add search for querying with pizza name, add toggle for checking details

--- frontend/src/app/shared/fronend-detail.ts
+++ frontend/src/app/shared/fronend-detail.ts
@@ -10,10 +10,20 @@ export class FrontendDetailService {
   url: string = environment.apiBaseUrl;
   constructor(private http: HttpClient) {}
 
-  getOrderDetails(page: number = 1, pageSize: number = 10) {
+  getOrderDetails(page: number, pageSize: number) {
     return this.http.get<Order[]>(
       `${this.url}/OrderDetails?page=${page}&pageSize=${pageSize}`
     );
+  }
+
+  searchOrders(query: string, page: number, pageSize: number) {
+    return this.http.get<Order[]>(
+      `${this.url}/OrderDetails/search?pizzaName=${query}&page=${page}&pageSize=${pageSize}`
+    );
+  }
+
+  getOrderDetailById(id: number) {
+    return this.http.get<Order>(`${this.url}/OrderDetails/${id}`);
   }
 
   getPizzaDetails(page: number = 1, pageSize: number = 10) {
