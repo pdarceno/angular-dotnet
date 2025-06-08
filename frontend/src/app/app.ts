924412e95ca7d1c905a3ce5ce365cf4eabe71029
Prosy Arceno <pdarceno@gmail.com>
2025-06-08 16:12:59 +0800

    feat: add pizzas dashboard, same logic with orders dashboard

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { OrderDetailsComponent } from './order-details/order-details';
-import { PizzaDetialsComponent } from './pizza-details/pizza-details';
+import { PizzaDetailsComponent } from './pizza-details/pizza-details';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [OrderDetailsComponent, PizzaDetialsComponent],
+  imports: [OrderDetailsComponent, PizzaDetailsComponent],
   templateUrl: './app.html',
 })
 export class App {}
