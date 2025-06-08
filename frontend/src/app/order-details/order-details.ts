08376ca4251f512e476a0dea32e1d533350dfb78
Prosy Arceno <pdarceno@gmail.com>
2025-06-08 14:07:39 +0800

    feat: move the assign logic to component details

--- frontend/src/app/order-details/order-details.ts
+++ frontend/src/app/order-details/order-details.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { FrontendDetailService } from '../shared/fronend-detail';
+import { CommonModule } from '@angular/common';
+import { Order } from '../shared/frontend-detail.model';
 
 @Component({
   selector: 'app-order-details',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './order-details.html',
   styles: ``,
 })
 export class OrderDetailsComponent implements OnInit {
   constructor(public service: FrontendDetailService) {}
-  ngOnInit() {
-    this.service.getOrderDetails();
+  orders: Order[] = [];
+
+  ngOnInit(): void {
+    this.service.getOrderDetails().subscribe({
+      next: (data) => {
+        this.orders = data;
+        console.log('Order data:', data);
+      },
+      error: (error) => {
+        console.error('Error:', error);
+      },
+    });
   }
 }
