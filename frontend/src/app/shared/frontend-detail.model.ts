ca168714db09d54eb4f8286ab48d768aae14a76e
Prosy Arceno <pdarceno@gmail.com>
2025-06-08 14:06:26 +0800

    feat: add models mapped to backend dto

---
+++ frontend/src/app/shared/frontend-detail.model.ts
@@ -0,0 +1,34 @@
+// mapped to backend's DTO
+export interface Order {
+  orderId: number;
+  date: string; // ISO date string (e.g., '2024-01-01')
+  time: string; // string representation of time (e.g., '12:30:00')
+  totalPrice: number;
+  pizzaCount: number;
+  details: OrderDetail[];
+}
+
+export interface OrderDetail {
+  orderDetailId: number;
+  pizzaName: string;
+  size: string;
+  quantity: number;
+  price: number;
+}
+
+export interface Pizza {
+  pizzaId: number;
+  pizzaTypeName: number;
+  category: string;
+  size: string;
+  price: number;
+  totalSold: number;
+}
+
+export interface PizzaType {
+  pizzaTypeId: number;
+  name: string;
+  category: string;
+  ingredients: string;
+  pizzas: Pizza[];
+}
