f0525ed65e5edbf4b46ef03c2b51a5149a4a8db0
Prosy Arceno <pdarceno@gmail.com>
2025-06-08 14:05:55 +0800

    feat: remove tight coupling of data fetch in service

--- frontend/src/app/shared/fronend-detail.ts
+++ frontend/src/app/shared/fronend-detail.ts
@@ -1,6 +1,7 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment.development';
+import { Order, Pizza } from './frontend-detail.model';
 
 @Injectable({
   providedIn: 'root',
@@ -10,24 +11,10 @@ export class FrontendDetailService {
   constructor(private http: HttpClient) {}
 
   getOrderDetails() {
-    this.http.get(this.url + '/OrderDetails').subscribe({
-      next: (data) => {
-        console.log('Data received:', data);
-      },
-      error: (error) => {
-        console.error('Error fetching data:', error);
-      },
-    });
+    return this.http.get<Order[]>(this.url + '/OrderDetails');
   }
 
   getPizzaDetails() {
-    this.http.get(this.url + '/Pizzas').subscribe({
-      next: (data) => {
-        console.log('Data received:', data);
-      },
-      error: (error) => {
-        console.error('Error fetching data:', error);
-      },
-    });
+    return this.http.get<Pizza[]>(this.url + '/Pizzas');
   }
 }
