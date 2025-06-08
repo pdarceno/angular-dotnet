5967c666c7552d800d28c43f8ac70d0851ee339d
Prosy Arceno <pdarceno@gmail.com>
2025-06-08 13:26:35 +0800

    style: transfered service out of model

--- frontend/src/app/shared/fronend-detail.ts
+++ frontend/src/app/shared/fronend-detail.ts
@@ -1,9 +1,33 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment.development';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class FronendDetail {
+export class FrontendDetailService {
+  url: string = environment.apiBaseUrl;
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getOrderDetails() {
+    this.http.get(this.url + '/OrderDetails').subscribe({
+      next: (data) => {
+        console.log('Data received:', data);
+      },
+      error: (error) => {
+        console.error('Error fetching data:', error);
+      },
+    });
+  }
+
+  getPizzaDetails() {
+    this.http.get(this.url + '/Pizzas').subscribe({
+      next: (data) => {
+        console.log('Data received:', data);
+      },
+      error: (error) => {
+        console.error('Error fetching data:', error);
+      },
+    });
+  }
 }
