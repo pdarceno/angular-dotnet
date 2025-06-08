18527754b7e27f6361bad93158a2c0db53a0b1bf
Prosy Arceno <pdarceno@gmail.com>
2025-06-08 11:15:29 +0800

    feat: add new DTOs for order and order details

---
+++ backend/backend/DTOs/OrderDto.cs
@@ -0,0 +1,10 @@
+﻿namespace backend.DTOs
+{
+    public class OrderDto
+    {
+        public int OrderId { get; set; }
+        public DateOnly Date { get; set; }
+        public TimeOnly Time { get; set; }
+        public List<OrderDetailDto> Details { get; set; } = new();
+    }
+}
